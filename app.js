cbd55e7765fa0e24498dbc451259da9c94df9958
Dhiman Sutradhar <dhimans7047@gmail.com>
2025-12-18 20:32:56 +0530

    extra commits

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ console.log("Welcome to Git Bash exploration!");
 
 // have created a function to demonstrate basic git commands
 // hello broother
+
+// it's a mistake 
+
+// function gitCommandsDemo() {
+//     console.log("This function will demonstrate basic git commands.");
+//     // Initialize a new git repository
