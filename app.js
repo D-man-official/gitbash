7569a5ff7c3f5297a0f9cb9181b531ff7104c923
Dhiman Sutradhar <dhimans7047@gmail.com>
2025-12-19 10:14:30 +0530

    remove eextra lines

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
 // i have created for expolring the features of gitbash
 console.log("Welcome to Git Bash exploration!");
 
-// have created a function to demonstrate basic git commands
-// hello broother
-
-// it's a mistake 
-
-// function gitCommandsDemo() {
-//     console.log("This function will demonstrate basic git commands.");
-//     // Initialize a new git repository
